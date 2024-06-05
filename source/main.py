97ded51d8d75ecf7f7951ceb951febc49267b7d3
sabufu <samuguiel@gmail.com>
2024-06-05 22:04:11 +0200

    comibacion de ramas y adaptación del código

--- source/main.py
+++ source/main.py
@@ -6,6 +6,14 @@ import os
 import test
 import tempfile
 import subprocess
+import re
+import sys
+
+import unittest
+import pytest
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import importlib.util
 
 load_dotenv()
 
@@ -15,11 +23,11 @@ api_key = os.environ.get('OPENAI_API_KEY')
 openai.api_key = api_key
 
 # Crear una fila para los logos en la esquina superior izquierda
-col1, col2, col3 = st.columns([1, 6, 1])
-with col1:
-    st.image('../images/soprasteria_logo.jpg', width=100)
-with col3:
-    st.image('../images/unir_universidad_internet_logo.jpg', width=100)
+# col1, col2, col3 = st.columns([1, 6, 1])
+# with col1:
+#     st.image('../images/soprasteria_logo.jpg', width=100)
+# with col3:
+#     st.image('../images/unir_universidad_internet_logo.jpg', width=100)
 
 # Título de la aplicación
 st.title('Generador de tests unitarios mediante IA Generativa')
@@ -34,11 +42,16 @@ st.write('''
 ''')
 st.write('Tutor: Pablo Guijarro')
 
+# Selector de framework de prueba
+test_framework = st.radio("Selecciona el framework para generar los tests:", ("unittest", "pytest"))
 
 def eliminar_caracteres_innecesarios(codigo):
     codigo = codigo.replace("```", "")
     codigo = codigo.replace("python", "")
-    return codigo
+    lineas = codigo.split('\n')
+    lineas = [linea for linea in lineas if not (linea.startswith('from my_module import') or linea.startswith('from mi_modulo import') or linea.startswith('from your_module import') or linea.startswith('from my_code import') or linea.startswith('from mycode import'))]
+    codigo_sin_import = '\n'.join(lineas)
+    return codigo_sin_import
 
 
 
@@ -52,25 +65,109 @@ def obtener_contenido_archivo_desde_github(url):
     else:
         return None
     
+def ejecutar_tests(tests_openai, test_framework):
+    # Guardar el código de los tests en un archivo temporal
+    with open("test_temp.py", "w") as f:
+        f.write(tests_openai)
+    
+    if test_framework == 'pytest':
+        # Ejecutar pytest
+        result = pytest.main(["test_temp.py"])
+        if result == 0:
+            st.info("Los tests se han ejecutado correctamente")
+        else:
+            st.error("Algunos tests fallaron, pero no se puede acceder a los errores con pytest para devolverlos a la api")
 
-# Se genera el código del test, se guarda en un archivo temporal y luego se ejecuta ese archivo dentro de la aplicación Streamlit, mostrando los resultados en la interfaz
-"""def ejecutar_test(codigo_test):
-    # Guardar el código del test en un archivo temporal
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp_file:
-        temp_file.write(codigo_test.encode('utf-8'))
-        temp_file.flush()
+    elif test_framework == 'unittest':
+        # Importar el módulo de tests temporalmente
+        spec = importlib.util.spec_from_file_location("test_temp", "test_temp.py")
+        test_temp = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(test_temp)
         
-        # Ejecutar el archivo de test y capturar la salida
-        resultado = subprocess.run(['python', temp_file.name], capture_output=True, text=True)
-        return resultado.stdout, resultado.stderr
+        # Crear un cargador de tests y una suite
+        loader = unittest.TestLoader()
+        suite = unittest.TestSuite()
+        
+        # Cargar tests desde el módulo importado
+        for name, obj in vars(test_temp).items():
+            if isinstance(obj, type) and issubclass(obj, unittest.TestCase):
+                suite.addTests(loader.loadTestsFromTestCase(obj))
+        
+        # Crear un runner para ejecutar los tests y capturar el resultado
+        runner = unittest.TextTestRunner(verbosity=2)
+        result = runner.run(suite)
+        
+        # Mostrar el resultado en Streamlit
+        if result.wasSuccessful():
+            st.info("Los tests se han ejecutado correctamente")
+        else:
+            st.error("Algunos tests fallaron, vamos a pasarle el código a la API a ver si puede corregirlo")
+            for error in result.errors:
+                st.error(f"Error: {error[1]}")
+            test_corr = test.get_tests_error(tests_openai, result.errors)
+            test_corr = eliminar_caracteres_innecesarios(test_corr)
+            ejecutar_tests_error(test_corr)
+    
+    # Eliminar el archivo temporal
+    os.remove("test_temp.py")
 
 
-def dividir_tests_en_funciones(codigo_test):
-    funciones = codigo_test.split("def ")
-    funciones = ["def " + f for f in funciones if f.strip()]
-    return funciones
-"""
+def ejecutar_tests_error(tests_openai):
+    # Guardar el código de los tests en un archivo temporal
+    with open("temp_tests_e.py", "w") as f:
+        f.write(tests_openai)
+    
+    # Importar el módulo de tests temporalmente
+    spec = importlib.util.spec_from_file_location("temp_tests_e", "temp_tests_e.py")
+    temp_tests = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(temp_tests)
+    
+    # Crear un cargador de tests y una suite
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    
+    # Cargar tests desde el módulo importado
+    for name, obj in vars(temp_tests).items():
+        if isinstance(obj, type) and issubclass(obj, unittest.TestCase):
+            suite.addTests(loader.loadTestsFromTestCase(obj))
+    
+    # Crear un runner para ejecutar los tests y capturar el resultado
+    runner = unittest.TextTestRunner(verbosity=2)
+    result = runner.run(suite)
+    
+    # Mostrar el resultado en Streamlit
+    if result.wasSuccessful():
+        st.info("Los tests se han ejecutado correctamente")
+    else:
+        st.error(f"Error al ejecutar los tests definitivos")
+        for error in result.errors:
+            st.error(f"Error: {error[1]}")
+            
+    # Eliminar el archivo temporal
+    os.remove("temp_tests_e.py")
 
+
+def ejecutar_contenido(contenido, tests_openai):
+    try:
+        exec(contenido)
+    except ModuleNotFoundError as e: #si hay módulos del script que no tenemos instalados, se instalan
+        modulo = re.search(r"No module named '([^']+)'", e.msg)
+        modulo = modulo.group(1)
+        subprocess.check_call([sys.executable, "-m", "pip", "install", modulo])
+        try:
+            exec(contenido)
+        except Exception as e:
+            st.error(f"Error al ejecutar el código del que dependen los tests: {e}")
+    except Exception as e:
+        st.error(f"Ha fallado el código con el siguiente error, pero se va a consultar de nuevo con openai: {e}")
+        try:
+            contenido = test.get_contenido_error(contenido, e.args[0])
+            contenido = eliminar_caracteres_innecesarios(contenido)
+            exec(contenido)
+        except:
+            st.error(f"Error al ejecutar el código que ha devuelto gpt: {e}")
+    contenido_y_tests = contenido + "\n" + tests_openai
+    return contenido_y_tests
 
 def main():
     # Crear pestañas
@@ -87,25 +184,28 @@ def main():
             with st.spinner('Generando tests unitarios...'):
                 if path:
                     contenido = obtener_contenido_archivo_desde_github(path)
+                    tests_openai = test.get_tests(contenido, test_framework)
+                    tests_openai = eliminar_caracteres_innecesarios(tests_openai)
                     if contenido:
-                        tests_openai = test.get_tests(contenido)
-                        test_unitario = eliminar_caracteres_innecesarios(tests_openai)
-
                         # Mostrar el contenido en un componente de Streamlit
-                        st.code(test_unitario, language="python")
+                        st.code(tests_openai, language="python")
+                        contenido_y_tests = ejecutar_contenido(contenido, tests_openai)
+                        ejecutar_tests(contenido_y_tests, test_framework)
 
-                                               # Botón de descarga
-                        st.download_button(
-                            label="Descargar test generado como archivo .py",
-                            data=test_unitario,
-                            file_name='test_unitario_generado.py',
-                            mime='text/plain'
-                        )
+                         #Esto todavía falla
+                        #ejecucion_tests =  exec(tests_openai) #Esto todavía falla
+                        #st.warning(ejecucion_tests)
                     else:
                         st.error("No se pudo obtener el contenido del archivo. Verifica los detalles del repositorio y la ruta del archivo.")
                 else:
-                    st.warning("Por favor, ingresa la URL del repositorio.")
-    
+                    st.warning("Por favor, ingresa todos los detalles del repositorio y la ruta del archivo.")
+                                               # Botón de descarg
+                st.download_button(
+                    label="Descargar test generado como archivo .py",
+                    data=tests_openai,
+                    file_name='test_unitario_generado.py',
+                    mime='text/plain'
+                )
     with tab2:
         st.header("Generar tests unitarios desde un archivo Python")
         
@@ -119,11 +219,13 @@ def main():
             st.code(code, language="python")
             if st.button('Generar tests unitarios'):
                 with st.spinner('Generando tests unitarios...'):
-                    tests_openai = test.get_tests(code)
+                    tests_openai = test.get_tests(code, test_framework)
                     test_unitario = eliminar_caracteres_innecesarios(tests_openai)
                     # Mostrar el contenido en un componente de Streamlit
                     st.write('Test Unitario generado:')
                     st.code(test_unitario, language="python")
+                    contenido_y_tests = ejecutar_contenido(code, test_unitario)
+                    ejecutar_tests(contenido_y_tests, test_framework)
                     
                 
                     
@@ -134,17 +236,6 @@ def main():
                         file_name='test_unitario_generado.py',
                         mime='text/plain'
                     )
-
-                # Botón para ejecutar el test
-                """ if st.button('Ejecutar test unitario'):
-                    with st.spinner('Ejecutando test unitario...'):
-                        salida, error = ejecutar_test(test_unitario)
-                        st.write('Resultado de la ejecución del test:')
-                        if salida:
-                            st.text_area('Salida', salida, height=200)
-                        if error:
-                            st.text_area('Error', error, height=200, text_color="red")
-                """
 
 if __name__ == "__main__":
     main()
--- source/test.py
+++ source/test.py
@@ -4,18 +4,65 @@ import os
 
 load_dotenv()
 
-def get_tests(code):
+def get_tests(code, test_framework):
   # Access the environment variables from the .env file
   api_key = os.environ.get('OPENAI_API_KEY')
 
   openai.api_key = api_key
+  client = openai.OpenAI()
+
+  if test_framework == "pytest":
+    prompt = "Genera los tests unitarios utilizando pytest para las funciones definidas en el siguiente código: " + code
+  elif test_framework == "unittest":
+    prompt = "Genera los tests unitarios utilizando unittest para las funciones definidas en el siguiente código: " + code
+  else:
+    raise ValueError("El framework de test especificado no es válido. Debe ser 'pytest' o 'unittest'.")
+
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "Solamente puedes devolver código en python, no devuelvas nada que no sea código python"},
+      {"role": "user", "content": prompt}
+    ]
+  )
+
+  print(completion.choices[0].message.content)
+  codigo = completion.choices[0].message.content
+  return codigo
+
+def get_contenido_error(code, error):
+  # Access the environment variables from the .env file
+  api_key = os.environ.get('OPENAI_API_KEY')
+
+  openai.api_key = api_key
+  client = openai.OpenAI()
 
 
-  completion = openai.ChatCompletion.create(
+  completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
       {"role": "system", "content": "Solamente puedes devolver código en python"},
-      {"role": "user", "content": "Genera la clase de tests unitarios para las funciones definidas en el siguiente código: " + code}
+      {"role": "user", "content": f"Ejecutando el siguiente código: ```{code}```. He obtenido el siguiente error: {error}. Devuelve solo el código corregido sin explicaciones."}
+    ]
+  )
+
+  print(completion.choices[0].message.content)
+  codigo = completion.choices[0].message.content
+  return codigo
+
+def get_tests_error(code, error):
+  # Access the environment variables from the .env file
+  api_key = os.environ.get('OPENAI_API_KEY')
+
+  openai.api_key = api_key
+  client = openai.OpenAI()
+
+
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "Solamente puedes devolver código en python"},
+      {"role": "user", "content": f"Ejecutando el siguiente código: ```{code}```. He obtenido el siguiente error: {error}. Devuelve solo el código corregido sin explicaciones."}
     ]
   )
 
