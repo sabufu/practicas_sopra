3d0272f77ecf975ca5f746e185b74119f10ab405
rafaalvear8 <78901156+rafaalvear8@users.noreply.github.com>
2024-06-05 21:12:03 +0200

    Add files via upload

---
+++ source/utils.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def descargar_archivo_contenido(file_name, content):
+    """
+    Muestra un botón de descarga en Streamlit para descargar un archivo con el contenido generado.
+
+    Args:
+        file_name (str): El nombre del archivo que se descargará.
+        content (str): El contenido del archivo a descargar.
+
+    Returns:
+        None
+    """
+    st.download_button(
+        label="Descargar test generado como archivo .py",
+        data=content,
+        file_name=file_name,
+        mime='text/plain'
+    )
